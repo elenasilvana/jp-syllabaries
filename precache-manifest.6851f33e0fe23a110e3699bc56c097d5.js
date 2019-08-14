d54728ec89f6280771b1b93e57a9d42ecd3bef4f
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2019-08-13 21:50:09 -0500

    Updates

---
+++ precache-manifest.6851f33e0fe23a110e3699bc56c097d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fd2c7a5905149f57efa96c398418ff",
+    "url": "/jp-syllabaries/index.html"
+  },
+  {
+    "revision": "350fdda08458088c41eb",
+    "url": "/jp-syllabaries/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9547a332bac8c0a204f3",
+    "url": "/jp-syllabaries/static/js/2.9e8de8f4.chunk.js"
+  },
+  {
+    "revision": "350fdda08458088c41eb",
+    "url": "/jp-syllabaries/static/js/main.9f59734c.chunk.js"
+  },
+  {
+    "revision": "396b2e915375416427b2",
+    "url": "/jp-syllabaries/static/js/runtime~main.4ea6f67f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jp-syllabaries/static/media/logo.5d5d9eef.svg"
+  }
+]);
