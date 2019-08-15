8ac4c3e2e653cf6f306d632a9db7107b7b6fe98a
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2019-08-15 17:12:16 -0500

    pages components created

---
+++ src/components/Syllabary/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Syllabary extends Component {
+    render(){
+        return(
+            <h1>Hola Syllabary</h1>
+        )
+    }
+}
