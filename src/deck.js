d0cba0581824b5eea2a854fe2220a07b7040e6e2
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2019-11-03 00:36:43 -0600

    implemented function for memory game to generate random deck from whole syllabary

--- src/deck.js
+++ src/deck.js
@@ -1,44 +1,49 @@
-import { KATAKANA } from './utils/syllabariesCharacters';
+import { KATAKANA } from "./utils/syllabariesCharacters";
+import Syllabary from "./utils/syllabary";
 
-
-function shuffle(array){
-    const _array = array.slice(0)
-    for(let i = 0; i < array.length -1; i++){
-        let randomIndex = Math.floor(Math.random() * (i + 1))
-        let temp = _array[i]
-        _array[i] = _array[randomIndex]
-        _array[randomIndex] = temp
-    }
-    return _array
+function shuffle(array) {
+  const _array = array.slice(0);
+  for (let i = 0; i < array.length - 1; i++) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    let temp = _array[i];
+    _array[i] = _array[randomIndex];
+    _array[randomIndex] = temp;
+  }
+  return _array;
 }
 
-export function getKatakanaSyllabary(){
-    let id = 0;
-    const SyllabariesChar = [...KATAKANA];
-    const katakanaCards = SyllabariesChar.reduce((acc, type)=>{
-        acc.push({
-            id: id++,
-            type
-        })
-        return acc
-    }, [])
-    return katakanaCards;
+export function getKatakanaSyllabary() {
+  let id = 0;
+  const SyllabariesChar = [...KATAKANA];
+  const katakanaCards = SyllabariesChar.reduce((acc, type) => {
+    acc.push({
+      id: id++,
+      type
+    });
+    return acc;
+  }, []);
+  return katakanaCards;
 }
 
-export function initializeDeck(){
-    let id = 0;
-    const cards = ['A', 'I', 'U', 'E', 'O', 'KA', 'KI', 'KU'
-].reduce((acc, type)=>{
-    acc.push({
-        id: id++,
-        type
-    })
-    acc.push({
-        id: id++,
-        type
-    })
-    return acc
-}, [])
+const CARDS_NUMBER = 16;
+export function initializeDeck() {
+  //función para generar baraja aleatoria a partir del silabario
+  const syllabary = Syllabary();
+  console.log("syllabary ", syllabary);
 
-return shuffle(cards);
+  let cards = [];
+  let id = 0;
+  while (cards.length < CARDS_NUMBER) {
+    const index = Math.floor(Math.random() * syllabary.length);
+    cards.push({
+      id: id++,
+      type: syllabary.splice(index, 1)[0]
+    });
+    cards.push({
+      id: id++,
+      type: syllabary.splice(index, 1)[0]
+    });
+    console.log(cards);
+  }
+  return shuffle(cards);
 }
