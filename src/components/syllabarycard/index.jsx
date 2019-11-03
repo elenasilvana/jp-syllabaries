58fba21aa259dac7a4b0921ded830e5d4557f937
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2019-11-03 00:58:06 -0600

    theres not real change on there

--- src/components/syllabarycard/index.jsx
+++ src/components/syllabarycard/index.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
-import propTypes from 'prop-types'
+import React from "react";
+import propTypes from "prop-types";
 
-import './styles.css'
+import "./styles.css";
 
-export default function SyllabaryCard ({
-    handleClick,
-    id,
-    type,
-    flipped,
-    height,
-    width
-}){
-    return (
-    <div className={`flip-container ${flipped ? 'flipped' : ''}`}
-    style={{
-        width, height
-    }}
-    onClick={() => handleClick(id)}
+export default function SyllabaryCard({
+  handleClick,
+  id,
+  type,
+  flipped,
+  height,
+  width
+}) {
+  return (
+    <div
+      className={`flip-container ${flipped ? "flipped" : ""}`}
+      style={{
+        width,
+        height
+      }}
+      onClick={() => handleClick(id)}
     >
-        <div className="flipper">
-            <img 
-            style={{
-                height, width
-            }}
-            className={flipped ? 'front' : 'back'}
-            src={flipped ? `${process.env.PUBLIC_URL}/img/katakana/${type}.png`: `${process.env.PUBLIC_URL}/img/katakana/${type}.png`}></img>
-
-        </div>
-
-    </div>)
+      <div className="flipper">
+        <img
+          style={{
+            height,
+            width
+          }}
+          className={flipped ? "front" : "back"}
+          src={
+            flipped
+              ? `${process.env.PUBLIC_URL}/img/katakana/${type}.png`
+              : `${process.env.PUBLIC_URL}/img/katakana/${type}.png`
+          }
+        ></img>
+      </div>
+    </div>
+  );
 }
 
 SyllabaryCard.propTypes = {
-    handleClick: propTypes.func.isRequired,
-    id: propTypes.number.isRequired,
-    flipped: propTypes.bool.isRequired,
-    type: propTypes.string.isRequired,
-    height: propTypes.number.isRequired,
-    width: propTypes.number.isRequired
-}
+  handleClick: propTypes.func.isRequired,
+  id: propTypes.number.isRequired,
+  flipped: propTypes.bool.isRequired,
+  type: propTypes.string.isRequired,
+  height: propTypes.number.isRequired,
+  width: propTypes.number.isRequired
+};
