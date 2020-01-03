8acb2cc55774736165b9b0f9fd032014ccafc74e
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2020-01-02 20:57:55 -0600

    Added buttons to switch between hiragana or katakana syllabary and implemented functionality

--- src/components/Syllabary/index.js
+++ src/components/Syllabary/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import SyllabaryBoard from "../syllabaryboard";
+import aKatakana from "../../layouts/akatakana.svg";
+import aHiragana from "../../layouts/ahiragana.svg";
 import "./styles.css";
 
 import * as Deck from "../../deck";
@@ -7,6 +9,8 @@ import * as Deck from "../../deck";
 export default function Syllabary() {
   const [cards, setCards] = useState([]);
   const [flipped, setFlipped] = useState([]);
+  const [isHiragana, setisHiragana] = useState(false);
+  const [isKatakana, setisKatakana] = useState(false);
 
   const handleClick = id => {
     if (flipped.length === 0) {
@@ -28,22 +32,51 @@ export default function Syllabary() {
   const sameCardClicked = id => flipped.includes(id);
 
   useEffect(() => {
-    setCards(Deck.getKatakanaSyllabary());
+    setCards(Deck.getSyllabaryDeck());
   }, []);
+
+  console.log("hiragana ", isHiragana, " katakana ", isKatakana);
 
   return (
     <div class="container">
       <div class="row">
         <div class="align-self-center justify-content-center">
           <h1 className="page-title">Katakana Syllabary</h1>
-          <SyllabaryBoard
-            cards={cards}
-            flipped={flipped}
-            handleClick={handleClick}
-          />
+          <div>
+            <button
+              className="icon-button"
+              onClick={() => {
+                setisKatakana(true);
+                if (isHiragana) {
+                  setisHiragana(false);
+                }
+              }}
+            >
+              <img src={aKatakana} style={{ width: "70px" }} />
+            </button>
+            <button
+              className="icon-button"
+              onClick={() => {
+                setisHiragana(true);
+                if (isKatakana) {
+                  setisKatakana(false);
+                }
+              }}
+            >
+              <img src={aHiragana} style={{ width: "70px" }} />
+            </button>
+          </div>
+          <div className={isHiragana || isKatakana ? "" : "hide"}>
+            <SyllabaryBoard
+              isHiragana={isHiragana}
+              isKatakana={isKatakana}
+              cards={cards}
+              flipped={flipped}
+              handleClick={handleClick}
+            />
+          </div>
         </div>
       </div>
     </div>
   );
-  // }
 }
