4ac9637b024affab51aa807f02e8b745411d0cfd
Elena Silvana Casillas <ele.silvana.c@gmail.com>
2020-11-16 15:26:37 -0600

    UI enhancements, some styles has been added to scss files

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -1,3 +1,23 @@
 import React from "react";
 
-export const About = () => <h1>ABOUT PAGE</h1>;
+export const About = () => {
+  return (
+    <div
+      className="about__container"
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
+      <div
+        className="about-description"
+        style={{ background: "aliceblue", width: "50%" }}
+      >
+        <h2>Project</h2>
+      </div>
+      <div
+        className="about-contact"
+        style={{ background: "grey", width: "50%" }}
+      >
+        <h2>Contact</h2>
+      </div>
+    </div>
+  );
+};
--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,3 +1,5 @@
 import React from "react";
 
+import "./home.scss";
+
 export const Home = () => <h1>HOME PAGE</h1>;
